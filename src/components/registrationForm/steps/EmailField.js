1fbf41cff28016ba2b750776a022e585f54e022d
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-05-29 15:41:54 +0545

    feat: verifies otp

--- src/components/registrationForm/steps/EmailField.js
+++ src/components/registrationForm/steps/EmailField.js
@@ -6,16 +6,28 @@ import "../../../pages/registrationForm/registrationForm.css";
 export default function EmailField() {
   const [email, setEmail] = useState("");
   const [btnText, setBtnText] = useState("NEXT");
+  const [emailErrorMsg, setEmailErrorMsg] = useState("");
+  const [serverErrorMsg, setServerErrorMsg] = useState("");
+
+  const isEmailValid = (email) => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+    
+  }
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setEmail(e.target.value);
+    if(!isEmailValid(e.target.value)){
+      setEmailErrorMsg("Please enter a valid email");
+    }else{
+      setEmailErrorMsg("");
+    }
   };
 
   const handleNextClick = (e) => {
-    if(!email){
-      alert("Email is invalid");
+    if(!isEmailValid(email)){
+      setEmailErrorMsg("Please enter a valid email");
       return;
     }
 
@@ -28,8 +40,14 @@ export default function EmailField() {
       navigate("/register/otp");
     })
     .catch(err => {
-      console.log("Error");
+      console.log(err.response.data.error);
       setBtnText("NEXT");
+      if(err.response.data.error){
+        setServerErrorMsg(err.response.data.error);
+      }else{
+        setServerErrorMsg("Something went wrong")
+      }
+      
   })
 
 
@@ -41,6 +59,7 @@ export default function EmailField() {
       <div className="container p-10 my-20">
         <div className="flex flex-col ">
           <div className="flex-1 w-full mx-2">
+          <p className="mb-4 text-red-500">{serverErrorMsg}</p>
             <div className="h-6 mt-3 text-xs font-bold leading-8 text-gray-500 uppercase">
               Email Address
             </div>
@@ -55,6 +74,7 @@ export default function EmailField() {
                 className="w-full p-1 px-2 text-gray-800 outline-none appearance-none"
               />
             </div>
+            <p className="mb-4 text-red-500">{emailErrorMsg}</p>
             <button
               onClick={() => handleNextClick()}
               className="px-4 py-2 mt-2 font-semibold text-white uppercase transition duration-200 ease-in-out bg-blue-500 rounded-lg cursor-pointer hover:bg-slate-700 hover:text-white"
--- src/components/registrationForm/steps/OtpField.js
+++ src/components/registrationForm/steps/OtpField.js
@@ -1,18 +1,51 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 export default function OtpField() {
 
   const [otp, setOtp] = useState("");
+  const [otpErrorMsg, setOtpErrorMsg] = useState("");
+  const [btnText, setBtnText] = useState("NEXT");
+  const [serverErrorMsg, setServerErrorMsg] = useState("");
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-   setOtp(e.target.value);
-  }
+    setServerErrorMsg("");
+    setOtp(e.target.value);
+    if(!e.target.value){
+      setOtpErrorMsg("Invalid otp");
+    }else{
+      setOtpErrorMsg("");
+    }
+  };
 
   const handleNextClick = (e) => {
-    alert(otp);
+    const email = "gyawali.rajbimal35@gmail.com"
+    if(!otp){
+      setOtpErrorMsg("Invalid otp");
+      return;
+    }
+
+    setBtnText("Loading...")
+
+    axios.post("http://localhost:8081/api/v1/verifyEmailOTP", {email, otp})
+    .then(res => {
+      console.log(res.data);
+      navigate("/register");
+    })
+    .catch(err => {
+      console.log(err.response.data.error);
+      setBtnText("NEXT");
+      if(err.response.data.error){
+        setServerErrorMsg(err.response.data.error);
+      }else{
+        setServerErrorMsg("Something went wrong")
+      }
+      
+  })
   }
 
   const handleBackClick = (e) => {
@@ -24,6 +57,7 @@ export default function OtpField() {
     <div className="container p-10 my-20">
     <div className="flex flex-col ">
       <div className="flex-1 w-full mx-2">
+      <p className="mb-4 text-red-500">{serverErrorMsg}</p>
         <div className="h-6 mt-3 text-xs font-bold leading-8 text-gray-500 uppercase">
           Verification
         </div>
@@ -39,6 +73,7 @@ export default function OtpField() {
             className="w-full p-1 px-2 text-gray-800 outline-none appearance-none"
           />
         </div>
+        <p className="mb-4 text-red-500">{otpErrorMsg}</p>
         <button
               onClick={() => handleBackClick()}
               className="px-4 py-2 mt-2 mr-2 font-semibold text-white uppercase transition duration-200 ease-in-out bg-gray-500 rounded-lg cursor-pointer hover:bg-slate-700 hover:text-white"
