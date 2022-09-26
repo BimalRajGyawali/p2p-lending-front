3f3497e6d85f35811ee457938eb365f68ea36887
Bimal Raj Gyawali <bimalraj.gyawali@codehimalaya.com>
2022-09-26 11:37:10 +0545

    feat: uses card for showing installments

--- src/components/repayment/ViewInstallments.js
+++ src/components/repayment/ViewInstallments.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {
+  CCard,
+  CCardBody,
+  CCardText,
   CTable,
   CTableBody,
   CTableDataCell,
@@ -20,7 +23,13 @@ const ViewInstallments = () => {
       paidDate: '2022-02-01',
     },
   ])
-  const [missedInstallments, setMissedInstallments] = useState([])
+  const [missedInstallments, setMissedInstallments] = useState([
+    {
+      id: '123',
+      amount: 4000,
+      scheduledDate: '2022-01-02',
+    },
+  ])
   const [paidInstallments, setPaidInstallments] = useState([
     {
       id: '123',
@@ -54,48 +63,79 @@ const ViewInstallments = () => {
         />
       </Link>
 
-      <CTable striped>
-        <CTableHead>
-          <CTableRow>
-            <CTableHeaderCell scope="col">#</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Loan Type</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Loan Duration</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Loan Amount</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Loan Status</CTableHeaderCell>
-            <CTableHeaderCell scope="col">Requested Date</CTableHeaderCell>
-          </CTableRow>
-        </CTableHead>
-        <CTableBody>
-          <CTableRow key={location.state.id}>
-            <CTableDataCell></CTableDataCell>
-            <CTableDataCell>{location.state.loanType}</CTableDataCell>
-            <CTableDataCell>{location.state.duration} months</CTableDataCell>
-            <CTableDataCell>{location.state.amount.toLocaleString('en-Us')}</CTableDataCell>
-            <CTableDataCell>{location.state.loanStatus}</CTableDataCell>
-            <CTableDataCell>{location.state.requestedDate}</CTableDataCell>
-          </CTableRow>
-        </CTableBody>
-      </CTable>
+      <CCard>
+        <CCardBody>
+          <CCardText>
+            <p style={{ fontWeight: '500', marginBottom: '25px' }}>Loan Details</p>
+            <CTable striped>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Loan Type</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Loan Duration</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Loan Amount</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Loan Status</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Requested Date</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableRow key={location.state.id}>
+                  <CTableDataCell></CTableDataCell>
+                  <CTableDataCell>{location.state.loanType}</CTableDataCell>
+                  <CTableDataCell>{location.state.duration} months</CTableDataCell>
+                  <CTableDataCell>{location.state.amount.toLocaleString('en-Us')}</CTableDataCell>
+                  <CTableDataCell>{location.state.loanStatus}</CTableDataCell>
+                  <CTableDataCell>{location.state.requestedDate}</CTableDataCell>
+                </CTableRow>
+              </CTableBody>
+            </CTable>
+          </CCardText>
+        </CCardBody>
+      </CCard>
 
       {scheduledInstallments.length > 0 && (
-        <div style={{ marginTop: '30px' }}>
-          <p style={{ fontWeight: '500' }}>Scheduled Installment</p>
-          <InstallmentTable installments={scheduledInstallments} type={installmentTypes.UNPAID} />
-        </div>
+        <CCard style={{ marginTop: '30px' }}>
+          <CCardBody>
+            <CCardText>
+              <div style={{ marginTop: '30px' }}>
+                <p style={{ fontWeight: '500', marginBottom: '25px' }}>Scheduled Installment</p>
+                <InstallmentTable
+                  installments={scheduledInstallments}
+                  type={installmentTypes.UNPAID}
+                />
+              </div>
+            </CCardText>
+          </CCardBody>
+        </CCard>
       )}
 
       {missedInstallments.length > 0 && (
-        <div style={{ marginTop: '30px' }}>
-          <p style={{ fontWeight: '500' }}>Missed Installment</p>
-          <InstallmentTable installments={missedInstallments} type={installmentTypes.UNPAID} />
-        </div>
+        <CCard style={{ marginTop: '30px' }}>
+          <CCardBody>
+            <CCardText>
+              <div style={{ marginTop: '30px' }}>
+                <p style={{ fontWeight: '500', marginBottom: '25px' }}>Missed Installment</p>
+                <InstallmentTable
+                  installments={missedInstallments}
+                  type={installmentTypes.UNPAID}
+                />
+              </div>
+            </CCardText>
+          </CCardBody>
+        </CCard>
       )}
 
       {paidInstallments.length > 0 && (
-        <div style={{ marginTop: '30px' }}>
-          <p style={{ fontWeight: '500' }}>Paid Installment</p>
-          <InstallmentTable installments={paidInstallments} type={installmentTypes.PAID} />
-        </div>
+        <CCard style={{ marginTop: '30px' }}>
+          <CCardBody>
+            <CCardText>
+              <div style={{ marginTop: '30px' }}>
+                <p style={{ fontWeight: '500', marginBottom: '25px' }}>Paid Installment</p>
+                <InstallmentTable installments={paidInstallments} type={installmentTypes.PAID} />
+              </div>
+            </CCardText>
+          </CCardBody>
+        </CCard>
       )}
     </>
   )
